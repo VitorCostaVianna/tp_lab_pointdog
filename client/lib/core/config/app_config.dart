85bc0f7df989088dec9d2e135faf981746210dd6
VitorCostaVianna <vcvianna@sga.pucminas.br>
2026-06-15 18:20:09 -0300

    fix: update baseUrl and wsUrl to correct server addresses

--- client/lib/core/config/app_config.dart
+++ client/lib/core/config/app_config.dart
@@ -1,10 +1,10 @@
 class AppConfig {
   static const String baseUrl = String.fromEnvironment(
     'BASE_URL',
-    defaultValue: 'http://10.0.2.2:3000',
+    defaultValue: 'http://10.221.32.78:3000',
   );
   static const String wsUrl = String.fromEnvironment(
     'WS_URL',
-    defaultValue: 'ws://10.0.2.2:3000/ws',
+    defaultValue: 'ws://10.221.32.78:3000/ws',
   );
 }
